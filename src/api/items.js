5546ca6eb734eec55efaab09c20a60b51f364fd6
eryk <kasumierka@gmail.com>
2018-01-08 20:33:39 +0100

    display similar items

--- src/api/items.js
+++ src/api/items.js
@@ -1,5 +1,9 @@
 import axios from 'axios/index'
 
-export const fetchItemAndSimilar = (slug, cb, cbError) => {
+export const fetchItem = (slug, cb, cbError) => {
   axios.get(`/api/item/${slug}`).then(cb, cbError)
+}
+
+export const fetchItemSimilar = (slug, cb, cbError) => {
+  axios.get(`/api/item/${slug}/similar`).then(cb, cbError)
 }
