b20c641c87aebe6f4df22cfe95ccadbe513f0a4b
eryk <kasumierka@gmail.com>
2018-01-09 17:21:18 +0100

    add itemHistory

--- src/store/index.js
+++ src/store/index.js
@@ -10,7 +10,7 @@ export const store = new Vuex.Store({
   plugins: [
     createPersistedState(
       {
-        paths: ['eq.eqItems', 'eq.canAddToEq']
+        paths: ['eq.eqItems', 'eq.canAddToEq', 'eq.itemHistory']
       }
     )
   ],
