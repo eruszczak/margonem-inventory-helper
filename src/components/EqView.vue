db65c514d8f4cf39e60281e5e82f2ccc9eb222a7
eryk <kasumierka@gmail.com>
2018-01-09 12:51:24 +0100

    add eqView

---
+++ src/components/EqView.vue
@@ -0,0 +1,26 @@
+<template>
+  <eq></eq>
+</template>
+
+<script>
+  import { mapGetters } from 'vuex'
+  import Eq from './components/Eq'
+
+  export default {
+    name: 'eq-view',
+    components: {
+      Eq
+    },
+    computed: {
+      ...mapGetters([
+        // map this.count to store.state.count
+        'pageTitle',
+        'canAddToEq'
+      ])
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
