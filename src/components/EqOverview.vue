9a0a89396f6c78fc15dcfedbe22a432aab8276c1
eryk <me@eryk.io>
2018-01-21 13:48:21 +0100

    overview

--- src/components/EqOverview.vue
+++ src/components/EqOverview.vue
@@ -1,14 +1,14 @@
 <template>
-    <div>
-      <b-tag type="is-dark is-medium" style="margin-top:10px;">{{ source.lvl }} lvl</b-tag>
-      <b-taglist v-if="!source.isConflict">
-        <b-tag v-if="source.allowedProfessions.length === 6" type="is-info">wszystkie profesje</b-tag>
-        <b-tag v-else v-for="prof in source.allowedProfessions" type="is-info">{{ prof | encodeProf }}</b-tag>
-      </b-taglist>
-      <div class="tags">
-        <span class="tag" v-for="rarity in ITEM_RARITY_IN_ORDER" :class="rarity + '-tag'">{{ source.rarity[rarity] || 0 }}</span>
-      </div>
+  <div>
+    <b-tag type="is-dark is-medium" style="margin-top:10px;">{{ source.lvl }} lvl</b-tag>
+    <b-taglist v-if="!source.isConflict">
+      <b-tag v-if="source.allowedProfessions.length === 6" type="is-info">wszystkie profesje</b-tag>
+      <b-tag v-else v-for="prof in source.allowedProfessions" type="is-info">{{ prof | encodeProf }}</b-tag>
+    </b-taglist>
+    <div class="tags">
+      <span class="tag" v-for="rarity in ITEM_RARITY_IN_ORDER" :class="rarity + '-tag'">{{ source.rarity[rarity] || 0 }}</span>
     </div>
+  </div>
 </template>
 
 <script>
