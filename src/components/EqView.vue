119e3593af192e8b7a5c4f01c6abefea49b2960f
eryk <kasumierka@gmail.com>
2018-01-21 14:05:56 +0100

    refactor components structure,

--- src/components/EqView.vue
+++ src/components/EqView.vue
@@ -13,54 +13,54 @@
     <eq :source="eqSet" :readOnly="readOnly"></eq>
     <button v-if="readOnly" class="button is-dark" @click="saveAsMine(eqItems)">Zapisz jako moje</button>
 
-    <p>TYLKO DO ODCZYTU. :</p>
-    <div class="columns">
-      <div class="column" v-for="eqItems in eqHistory">
-        <eq :source="eqItems" :readOnly="true"></eq>
-        <span>kopiuj link</span>
-        <router-link :to="getEqLink(eqItems)">Przejdź do zestawu</router-link>
-        <button class="button is-dark" @click="saveAsMine(eqItems)">Zapisz jako moje</button>
-      </div>
-    </div>
+    <!--<p>TYLKO DO ODCZYTU. :</p>-->
+    <!--<div class="columns">-->
+      <!--<div class="column" v-for="eqItems in eqHistory">-->
+        <!--<eq :source="eqItems" :readOnly="true"></eq>-->
+        <!--<span>kopiuj link</span>-->
+        <!--<router-link :to="getEqLink(eqItems)">Przejdź do zestawu</router-link>-->
+        <!--<button class="button is-dark" @click="saveAsMine(eqItems)">Zapisz jako moje</button>-->
+      <!--</div>-->
+    <!--</div>-->
 
-    <section>
-      <b-collapse class="card" :open.sync="isOpen">
-        <div slot="trigger" class="card-header">
-          <p class="card-header-title">
-            Ostatnio przeglądane zestawy {{ eqHistory.length }}
-          </p>
-          <!--<a class="card-header-icon">-->
-            <!--<b-icon :icon="isOpen ? 'menu-down' : 'menu-up'"></b-icon>-->
-          <!--</a>-->
-        </div>
-        <div class="card-content">
-          <div class="content">
-            Możesz zobaczyć 5 ostatnich odwiedzanych zestawów.
-            <button class="button is-dark" @click="showNext">Następny</button>
-            <eq :source="currentEqItems" :readOnly="true"></eq>
-          </div>
-        </div>
-        <footer class="card-footer">
-          <button class="card-footer-item">kopiuj link</button>
-          <router-link class="card-footer-item" :to="getEqLink(currentEqItems)">Przejdź do zestawu</router-link>
-          <button class="card-footer-item" @click="saveAsMine(currentEqItems)">Zapisz jako moje</button>
-        </footer>
-      </b-collapse>
-    </section>
+    <!--<section>-->
+      <!--<b-collapse class="card" :open.sync="isOpen">-->
+        <!--<div slot="trigger" class="card-header">-->
+          <!--<p class="card-header-title">-->
+            <!--Ostatnio przeglądane zestawy {{ eqHistory.length }}-->
+          <!--</p>-->
+          <!--&lt;!&ndash;<a class="card-header-icon">&ndash;&gt;-->
+            <!--&lt;!&ndash;<b-icon :icon="isOpen ? 'menu-down' : 'menu-up'"></b-icon>&ndash;&gt;-->
+          <!--&lt;!&ndash;</a>&ndash;&gt;-->
+        <!--</div>-->
+        <!--<div class="card-content">-->
+          <!--<div class="content">-->
+            <!--Możesz zobaczyć 5 ostatnich odwiedzanych zestawów.-->
+            <!--<button class="button is-dark" @click="showNext">Następny</button>-->
+            <!--<eq :source="currentEqItems" :readOnly="true"></eq>-->
+          <!--</div>-->
+        <!--</div>-->
+        <!--<footer class="card-footer">-->
+          <!--<button class="card-footer-item">kopiuj link</button>-->
+          <!--<router-link class="card-footer-item" :to="getEqLink(currentEqItems)">Przejdź do zestawu</router-link>-->
+          <!--<button class="card-footer-item" @click="saveAsMine(currentEqItems)">Zapisz jako moje</button>-->
+        <!--</footer>-->
+      <!--</b-collapse>-->
+    <!--</section>-->
 
-    <b-tabs v-model="current">
-      <b-tab-item label="Pictures">
-        <eq :source="currentEqItems" :readOnly="true"></eq>
-      </b-tab-item>
+    <!--<b-tabs v-model="current">-->
+      <!--<b-tab-item label="Pictures">-->
+        <!--<eq :source="currentEqItems" :readOnly="true"></eq>-->
+      <!--</b-tab-item>-->
 
-      <b-tab-item label="Music">
-        Lorem <br>
-        ipsum <br>
-        dolor <br>
-        sit <br>
-        amet.
-      </b-tab-item>
-    </b-tabs>
+      <!--<b-tab-item label="Music">-->
+        <!--Lorem <br>-->
+        <!--ipsum <br>-->
+        <!--dolor <br>-->
+        <!--sit <br>-->
+        <!--amet.-->
+      <!--</b-tab-item>-->
+    <!--</b-tabs>-->
     <!-- <section class="section">
       <eq-summary :source="eqSetStats"></eq-summary>
     </section> -->
@@ -71,13 +71,12 @@
   import { mapGetters, mapActions, mapMutations } from 'vuex'
   import Eq from './Eq'
   import RestoreEq from './RestoreEq'
-  import EqSummary from './EqSummary'
   import { getEqRoute } from '../utils/helpers'
   import { toast } from '../mixins/toast'
 
   export default {
     name: 'eq-view',
-    components: {Eq, EqSummary, RestoreEq},
+    components: {Eq, RestoreEq},
     data () {
       return {
         isOpen: true,
--- src/components/includes/EqOverview.vue
+++ src/components/includes/EqOverview.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
     <b-tag type="is-dark is-medium" style="margin-top:10px;">{{ source.lvl }} lvl</b-tag>
-    <b-taglist v-if="!source.isConflict">
-      <b-tag v-if="source.allowedProfessions.length === 6" type="is-info">wszystkie profesje</b-tag>
+    <b-taglist>
+      <b-tag v-if="source.isConflict" type="is-danger">sprzeczne typy przedmiotów</b-tag>
+      <b-tag v-else-if="source.allowedProfessions.length === 6" type="is-info">wszystkie profesje</b-tag>
       <b-tag v-else v-for="prof in source.allowedProfessions" type="is-info">{{ prof | encodeProf }}</b-tag>
     </b-taglist>
     <div class="tags">
@@ -12,7 +13,7 @@
 </template>
 
 <script>
-  import { ITEM_BONUS, ITEM_BONUSES_IN_ORDER, ITEM_RARITY_IN_ORDER, CHARACTER_CLASSES } from '../utils/items'
+  import { ITEM_RARITY_IN_ORDER, CHARACTER_CLASSES } from '../../utils/items'
 
   export default {
     name: 'eq-overview',
