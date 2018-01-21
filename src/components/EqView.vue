e8dabfd189dd89e4d3fc8b2a054c51b38eb32dc9
eryk <me@eryk.io>
2018-01-21 15:44:31 +0100

    add footer

--- src/components/EqView.vue
+++ src/components/EqView.vue
@@ -1,111 +1,117 @@
 <template>
   <section class="section">
     <div class="container">
-    <div class="message is-info">
-      <div class="message-body">
-        <p>Aby szybko stworzyć zestaw, znajdź przedmioty za pomocą wyszukiwarki powyżej oraz załóż je klikając na nie PPM.</p>
+      <div class="message is-info">
+        <div class="message-body">
+          <p>Aby szybko stworzyć zestaw, znajdź przedmioty za pomocą wyszukiwarki powyżej oraz załóż je klikając na nie PPM.</p>
+        </div>
       </div>
-    </div>
 
-    <div class="tile is-ancestor">
-      <div class="tile is-vertical is-7">
-        <div class="tile">
-          <div class="tile is-parent">
-            <article class="tile is-child notification is-info has-text-centered">
-              <p class="title">Ekwipunek</p>
-              <div class="content">
-                <eq :source="eqSet" :readOnly="readOnly"></eq>
-                <button v-if="readOnly" class="button is-dark" @click="saveAsMine(eqItems)">Zapisz jako moje</button>
-              </div>
-            </article>
+      <div class="tile is-ancestor">
+        <div class="tile is-vertical is-7">
+          <div class="tile">
+            <div class="tile is-parent">
+              <article class="tile is-child notification is-info has-text-centered">
+                <p class="title">Ekwipunek</p>
+                <div class="content">
+                  <eq :source="eqSet" :readOnly="readOnly"></eq>
+                  <button v-if="readOnly" class="button is-dark" @click="saveAsMine(eqItems)">Zapisz jako moje</button>
+                </div>
+              </article>
+            </div>
+            <div class="tile is-parent">
+              <article class="tile is-child notification is-warning has-text-centered">
+                <p class="title">Podsumowanie</p>
+                <div class="content">
+                  <eq-overview :source="eqSetStats"></eq-overview>
+                  <p v-if="readOnly"><b>TYLKO DO ODCZYTU</b></p>
+                  <div v-if="!readOnly">
+                    <restore-eq></restore-eq>
+                    <button class="button is-dark" @click="restart">restart</button>
+                  </div>
+                </div>
+              </article>
+            </div>
           </div>
           <div class="tile is-parent">
-            <article class="tile is-child notification is-warning has-text-centered">
-              <p class="title">Podsumowanie</p>
+            <article class="tile is-child notification is-danger has-text-centered">
+              <p class="title">Bonusy</p>
               <div class="content">
-                <eq-overview :source="eqSetStats"></eq-overview>
-                <p v-if="readOnly"><b>TYLKO DO ODCZYTU</b></p>
-                <div v-if="!readOnly">
-                  <restore-eq></restore-eq>
-                  <button class="button is-dark" @click="restart">restart</button>
-                </div>
+                <eq-bonuses :source="eqSetStats"></eq-bonuses>
               </div>
             </article>
           </div>
         </div>
         <div class="tile is-parent">
-          <article class="tile is-child notification is-danger has-text-centered">
-            <p class="title">Bonusy</p>
+          <article class="tile is-child notification is-success has-text-centered">
             <div class="content">
-              <eq-bonuses :source="eqSetStats"></eq-bonuses>
+              <p class="title">Statystyki</p>
+              <div class="content">
+                <eq-stats :source="eqSetStats"></eq-stats>
+              </div>
             </div>
           </article>
         </div>
       </div>
-      <div class="tile is-parent">
-        <article class="tile is-child notification is-success has-text-centered">
+      <article class="tile is-child notification is-primary has-text-centered">
+        <div class="content">
+          <p class="title">Ostatnio przeglądane zestawy</p>
           <div class="content">
-            <p class="title">Statystyki</p>
-            <div class="content">
-              <eq-stats :source="eqSetStats"></eq-stats>
-            </div>
           </div>
-        </article>
-      </div>
-    </div>
-
-    <!-- </template> -->
-    <!--<p>TYLKO DO ODCZYTU. :</p>-->
-    <!--<div class="columns">-->
+        </div>
+      </article>
+      <!-- </template> -->
+      <!--<p>TYLKO DO ODCZYTU. :</p>-->
+      <!--<div class="columns">-->
       <!--<div class="column" v-for="eqItems in eqHistory">-->
-        <!--<eq :source="eqItems" :readOnly="true"></eq>-->
-        <!--<span>kopiuj link</span>-->
-        <!--<router-link :to="getEqLink(eqItems)">Przejdź do zestawu</router-link>-->
-        <!--<button class="button is-dark" @click="saveAsMine(eqItems)">Zapisz jako moje</button>-->
+      <!--<eq :source="eqItems" :readOnly="true"></eq>-->
+      <!--<span>kopiuj link</span>-->
+      <!--<router-link :to="getEqLink(eqItems)">Przejdź do zestawu</router-link>-->
+      <!--<button class="button is-dark" @click="saveAsMine(eqItems)">Zapisz jako moje</button>-->
       <!--</div>-->
-    <!--</div>-->
+      <!--</div>-->
 
-    <!--<section>-->
+      <!--<section>-->
       <!--<b-collapse class="card" :open.sync="isOpen">-->
-        <!--<div slot="trigger" class="card-header">-->
-          <!--<p class="card-header-title">-->
-            <!--Ostatnio przeglądane zestawy {{ eqHistory.length }}-->
-          <!--</p>-->
-          <!--&lt;!&ndash;<a class="card-header-icon">&ndash;&gt;-->
-            <!--&lt;!&ndash;<b-icon :icon="isOpen ? 'menu-down' : 'menu-up'"></b-icon>&ndash;&gt;-->
-          <!--&lt;!&ndash;</a>&ndash;&gt;-->
-        <!--</div>-->
-        <!--<div class="card-content">-->
-          <!--<div class="content">-->
-            <!--Możesz zobaczyć 5 ostatnich odwiedzanych zestawów.-->
-            <!--<button class="button is-dark" @click="showNext">Następny</button>-->
-            <!--<eq :source="currentEqItems" :readOnly="true"></eq>-->
-          <!--</div>-->
-        <!--</div>-->
-        <!--<footer class="card-footer">-->
-          <!--<button class="card-footer-item">kopiuj link</button>-->
-          <!--<router-link class="card-footer-item" :to="getEqLink(currentEqItems)">Przejdź do zestawu</router-link>-->
-          <!--<button class="card-footer-item" @click="saveAsMine(currentEqItems)">Zapisz jako moje</button>-->
-        <!--</footer>-->
+      <!--<div slot="trigger" class="card-header">-->
+      <!--<p class="card-header-title">-->
+      <!--Ostatnio przeglądane zestawy {{ eqHistory.length }}-->
+      <!--</p>-->
+      <!--&lt;!&ndash;<a class="card-header-icon">&ndash;&gt;-->
+      <!--&lt;!&ndash;<b-icon :icon="isOpen ? 'menu-down' : 'menu-up'"></b-icon>&ndash;&gt;-->
+      <!--&lt;!&ndash;</a>&ndash;&gt;-->
+      <!--</div>-->
+      <!--<div class="card-content">-->
+      <!--<div class="content">-->
+      <!--Możesz zobaczyć 5 ostatnich odwiedzanych zestawów.-->
+      <!--<button class="button is-dark" @click="showNext">Następny</button>-->
+      <!--<eq :source="currentEqItems" :readOnly="true"></eq>-->
+      <!--</div>-->
+      <!--</div>-->
+      <!--<footer class="card-footer">-->
+      <!--<button class="card-footer-item">kopiuj link</button>-->
+      <!--<router-link class="card-footer-item" :to="getEqLink(currentEqItems)">Przejdź do zestawu</router-link>-->
+      <!--<button class="card-footer-item" @click="saveAsMine(currentEqItems)">Zapisz jako moje</button>-->
+      <!--</footer>-->
       <!--</b-collapse>-->
-    <!--</section>-->
+      <!--</section>-->
 
-    <!--<b-tabs v-model="current">-->
+      <!--<b-tabs v-model="current">-->
       <!--<b-tab-item label="Pictures">-->
-        <!--<eq :source="currentEqItems" :readOnly="true"></eq>-->
+      <!--<eq :source="currentEqItems" :readOnly="true"></eq>-->
       <!--</b-tab-item>-->
 
       <!--<b-tab-item label="Music">-->
-        <!--Lorem <br>-->
-        <!--ipsum <br>-->
-        <!--dolor <br>-->
-        <!--sit <br>-->
-        <!--amet.-->
+      <!--Lorem <br>-->
+      <!--ipsum <br>-->
+      <!--dolor <br>-->
+      <!--sit <br>-->
+      <!--amet.-->
       <!--</b-tab-item>-->
-    <!--</b-tabs>-->
-    <!-- <section class="section">
-      <eq-summary :source="eqSetStats"></eq-summary>
-    </section> -->
+      <!--</b-tabs>-->
+      <!-- <section class="section">
+        <eq-summary :source="eqSetStats"></eq-summary>
+      </section> -->
     </div>
   </section>
 </template>
