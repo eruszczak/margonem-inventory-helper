36a9b1c98af43004564ebc649a60da353699b833
eryk <me@eryk.io>
2018-01-24 11:29:14 +0100

    clear stack when replacing eq

--- src/components/ItemCompareView.vue
+++ src/components/ItemCompareView.vue
@@ -31,6 +31,39 @@
           <!--skopiuj link-->
         <!--</v-btn>-->
       <!--</template>-->
+      <!--<v-expansion-panel expand>-->
+        <!--<template v-for="(pair, index) in pairs">-->
+          <!--<v-expansion-panel-content class="pt-2 pb-2">-->
+            <!--<div slot="header">-->
+              <!--<item :stats="pair.left" @rightClick="removePairsWithThisItem"></item> vs <item :stats="pair.right" @rightClick="removePairsWithThisItem"></item>-->
+            <!--</div>-->
+            <!--<v-card>-->
+              <!--<v-row>-->
+                <!--<v-col md6 offset-md3>-->
+                  <!--<div class="text-xs-center">-->
+                    <!--&lt;!&ndash; <b>{{ pair.left.name }}</b> vs <b>{{ pair.right.name }}</b> &ndash;&gt;-->
+                    <!--&lt;!&ndash; <item :stats="pair.left" @rightClick="removePairsWithThisItem"></item> &ndash;&gt;-->
+                    <!--&lt;!&ndash; <item :stats="pair.right" @rightClick="removePairsWithThisItem"></item> &ndash;&gt;-->
+                    <!--&lt;!&ndash; <button @click="pair.show = !pair.show">{{ pair.show ? 'Ukryj szczegóły' : 'Pokaż szczegóły' }}</button> &ndash;&gt;-->
+                    <!--&lt;!&ndash; <v-btn @click.native="pairs.splice(index, 1)" small round error dark>-->
+                      <!--Usuń parę-->
+                    <!--</v-btn> &ndash;&gt;-->
+
+                    <!--<eq-set-summary-->
+                      <!--v-show="!pair.show"-->
+                      <!--:leftOne="pair.left"-->
+                      <!--:rightOne="pair.right"-->
+                      <!--:displayBothValues="true"-->
+                      <!--:hideSummary="true"-->
+                    <!--&gt;-->
+                    <!--</eq-set-summary>-->
+                  <!--</div>-->
+                <!--</v-col>-->
+              <!--</v-row>-->
+            <!--</v-card>-->
+          <!--</v-expansion-panel-content>-->
+        <!--</template>-->
+      <!--</v-expansion-panel>-->
     </div>
 
   </section>
