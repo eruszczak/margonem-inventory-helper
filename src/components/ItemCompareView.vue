c5310c445051b8ef3dc15e4aa6add2ed06bc4e38
eryk <kasumierka@gmail.com>
2018-01-29 11:07:12 +0100

    fix comparison

--- src/components/ItemCompareView.vue
+++ src/components/ItemCompareView.vue
@@ -17,35 +17,37 @@
         </div>
       </div>
       <button class="button" @click="removeAllItems">Wyczyść przedmioty</button>
-{{comparision}}
-      <template v-for="type in comparision">
+<!--{{comparision}}-->
+      <template v-for="(items, key) in comparision" v-if="items">
         <section class="hero is-primary">
           <div class="hero-body">
             <div class="container">
               <h1 class="title">
-                {{ type }}
+                {{ key }}
               </h1>
               <h2 class="subtitle">
-                <item v-for="pk in comparision[type]" :key="pk" :data="comparision[type][pk].item" :action="rmbActions.removeCompare"/>
+                <!--{{// comparision[key]}}-->
+                <!--<p v-for="(val2, key2) in comparision[key]">{{comparision[key][key2]}}</p>-->
+                <item v-for="(val2, key2) in comparision[key]" :key="pk" :data="comparision[key][key2].item" :action="rmbActions.removeCompare"/>
               </h2>
             </div>
           </div>
         </section>
           <!--<item :data="pair.item"/>-->
-        <tabs size="is-small" class="block">
-          <tab-item v-for="pk in comparision[type]" :item="comparision[type][pk].item">
-            <tabs size="is-small" class="block">
-              <tab-item v-for="comparision in comparision[type][pk].comparisions" :item="comparision.item">
-                <!--<div slot="label">-->
-                <!--<img class="itemborder borderRarity" :class="comparision.item.rarity" :src="comparision.item.img" :alt="comparision.item.name">-->
-                <!--<p>{{ comparision.item.name }}</p>-->
-                <!--</div>-->
-                <item :data="comparision[type][pk].item"/> vs <item :data="comparision.item"/>
-                <!--<eq-stats-compare :leftSource="pair.itemStats" :rightSource="comparision.itemStats"/>-->
-              </tab-item>
-            </tabs>
-          </tab-item>
-        </tabs>
+        <!--<tabs size="is-small" class="block">-->
+          <!--<tab-item v-for="pk in comparision[type]" :item="comparision[type][pk].item">-->
+            <!--<tabs size="is-small" class="block">-->
+              <!--<tab-item v-for="comparision in comparision[type][pk].comparisions" :item="comparision.item">-->
+                <!--&lt;!&ndash;<div slot="label">&ndash;&gt;-->
+                <!--&lt;!&ndash;<img class="itemborder borderRarity" :class="comparision.item.rarity" :src="comparision.item.img" :alt="comparision.item.name">&ndash;&gt;-->
+                <!--&lt;!&ndash;<p>{{ comparision.item.name }}</p>&ndash;&gt;-->
+                <!--&lt;!&ndash;</div>&ndash;&gt;-->
+                <!--<item :data="comparision[type][pk].item"/> vs <item :data="comparision.item"/>-->
+                <!--&lt;!&ndash;<eq-stats-compare :leftSource="pair.itemStats" :rightSource="comparision.itemStats"/>&ndash;&gt;-->
+              <!--</tab-item>-->
+            <!--</tabs>-->
+          <!--</tab-item>-->
+        <!--</tabs>-->
       </template>
     </div>
   </section>
