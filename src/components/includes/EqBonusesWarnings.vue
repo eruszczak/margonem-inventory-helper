60c534da8954c2d73c3df90c4a56d84ffd3ab842
eryk <me@eryk.io>
2018-01-30 12:51:56 +0100

    bonus warnings

--- src/components/includes/EqBonusesWarnings.vue
+++ src/components/includes/EqBonusesWarnings.vue
@@ -1,8 +1,9 @@
 <template>
   <div v-if="source.bonusWarnings.decreased.length || isAnyLimitReached">
     <hr style="border:0">
+    {{source.bonusWarnings}}
     <b-message title="Uwagi do bonusów" type="is-warning" :closable="false">
-      <ul>
+      <ul v-if="isWarnings">
         <li v-for="item in source.bonusWarnings.decreased">{{ item }}</li>
         <li v-for="(value, key) in source.bonusWarnings.limit">Przekroczono limit: {{ key }} (x{{ value }})</li>
         <li v-if="isAnyLimitReached"><small>Wartości oraz uwagi dla bonusu z przekroczonym limitem mogą być niepoprawne.</small></li>
@@ -20,6 +21,9 @@
     computed: {
       isAnyLimitReached: function () {
         return !isObjEmpty(this.source.bonusWarnings.limit)
+      },
+      isWarnings: function () {
+        return !isObjEmpty(this.source.bonusWarnings)
       }
     }
   }
