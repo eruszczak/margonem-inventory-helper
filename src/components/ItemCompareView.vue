510939c49fe2daa9b23a8f1e03e63132eb61a067
eryk <kasumierka@gmail.com>
2018-01-30 09:32:15 +0100

    ignore action

--- src/components/ItemCompareView.vue
+++ src/components/ItemCompareView.vue
@@ -30,7 +30,7 @@
         <tabs size="is-small" class="block">
           <tab-item slot="content" v-for="(comp, index) in items" :key="index" :item="comp.item" :noLink="true" :itemAction="RIGHT_CLICK_MAPPER.removeCompare">
             <tabs size="is-small" class="block">
-              <tab-item slot="content" v-for="(comparison, index) in comp.comparisons" :key="index" :noLink="true" :item="comparison.item" label="vs">
+              <tab-item slot="content" v-for="(comparison, index) in comp.comparisons" :key="index" :noLink="true" :itemAction="RIGHT_CLICK_MAPPER.ignore" :item="comparison.item" label="vs">
                 <eq-stats-compare :leftSource="comp.itemStats" :rightSource="comparison.itemStats"/>
               </tab-item>
             </tabs>
