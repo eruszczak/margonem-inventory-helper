62ffa6a52a2994aff9197af42fba43069c051405
eryk <me@eryk.io>
2018-02-01 10:36:02 +0100

    install bulma, replace buefy css with bulma,

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
 import Buefy from 'buefy'
-import 'buefy/lib/buefy.css'
+import 'bulma/css/bulma.css' // todo modules
 import { store } from './store/index'
 import VueClipboard from 'vue-clipboard2'
 
