2c90c316e4b2a73599028bec13ca0e4b84bb2e19
eryk <kasumierka@gmail.com>
2018-02-04 10:23:17 +0100

    comment out: itemCompareView, eqHistory

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,11 @@
       <div class="navbar-brand">
         <router-link class="navbar-item" :to="{name: 'items'}">Przedmioty</router-link>
         <router-link class="navbar-item" :to="{name: 'eqView'}">Zestawy EQ</router-link>
-        <router-link class="navbar-item" :to="{name: 'itemCompareView'}">Porównaj</router-link>
+        <!--<router-link class="navbar-item" :to="{name: 'itemCompareView'}">Porównaj</router-link>-->
         <a class="button is-primary is-medium" :class="{'is-active': modalActive}" @click="toggleModal">Pokaż EQ</a>
-        <toggle class="navbar-item" :value="canAddToEq" @input="toggleCanAddToEq">
-          {{ canAddToEq ? 'Do eq' : 'Do porównywarki'}}
-        </toggle>
+        <!--<toggle class="navbar-item" :value="canAddToEq" @input="toggleCanAddToEq">-->
+          <!--{{ canAddToEq ? 'Do eq' : 'Do porównywarki'}}-->
+        <!--</toggle>-->
         <my-input :value="searchQuery" @input="setSearchQuery" placeholder="Szukaj przedmiotów"></my-input>
       </div>
     </nav>
