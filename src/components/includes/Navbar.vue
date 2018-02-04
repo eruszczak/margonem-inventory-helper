1ac6c79c21456eaecca527ad5ef2e9397bf68431
eryk <me@eryk.io>
2018-02-04 12:01:20 +0100

    fix navbar: add navbar-menu

--- src/components/includes/Navbar.vue
+++ src/components/includes/Navbar.vue
@@ -1,14 +1,30 @@
 <template>
   <nav class="navbar is-fixed-top" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
+      <router-link class="navbar-item" to="/">
+        <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
+      </router-link>
+      <button class="button navbar-burger" @click="isActive = !isActive">
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+    </div>
+    <div class="navbar-menu" :class="{'is-active': isActive}">
       <router-link class="navbar-item" :to="{name: 'items'}">Przedmioty</router-link>
       <router-link class="navbar-item" :to="{name: 'eqView'}">Zestawy EQ</router-link>
       <!--<router-link class="navbar-item" :to="{name: 'itemCompareView'}">Porównaj</router-link>-->
-      <a class="button is-info is-medium" :class="{'is-active': modalActive}" @click="toggleModal">Pokaż EQ</a>
+      <div class="navbar-item field is-grouped is-grouped-multiline">
+      <p class="control">
+        <a class="button is-info" :class="{'is-active': modalActive}" @click="toggleModal">Pokaż EQ</a>
+      </p>
+        <p class="control">
+          <my-input :value="searchQuery" @input="setSearchQuery" placeholder="Szukaj przedmiotów"/>
+        </p>
+      </div>
       <!--<toggle class="navbar-item" :value="canAddToEq" @input="toggleCanAddToEq">-->
       <!--{{ canAddToEq ? 'Do eq' : 'Do porównywarki'}}-->
       <!--</toggle>-->
-      <my-input :value="searchQuery" @input="setSearchQuery" placeholder="Szukaj przedmiotów"></my-input>
     </div>
   </nav>
 </template>
@@ -18,6 +34,11 @@
 
   export default {
     name: 'navbar',
+    data () {
+      return {
+        isActive: false
+      }
+    },
     computed: {
       ...mapGetters(['searchQuery', 'modalActive'])
     },
