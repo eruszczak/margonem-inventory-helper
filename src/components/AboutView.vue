e4885c70547190e5f62a9422f242de83d687f166
eryk <me@eryk.io>
2018-02-10 18:24:17 +0100

    add containers and sections

--- src/components/AboutView.vue
+++ src/components/AboutView.vue
@@ -1,22 +1,24 @@
 <template>
-  <div class="container mt1">
-    <msg header="O stronie">
-      <ul>
-        <li>można przeglądać i wyszukiwać przedmioty z gry Margonem</li>
-        <li>można budować swój ekwipunek</li>
-        <li>można udostępniać swój ekwipunek poprzez link</li>
-        <li>można porównać swój ekwipunek z odwiedzanym</li>
-      </ul>
-    </msg>
-    <msg header="Nadchodzące zmiany">
-      <ul>
-        <li>porównywarka przedmiotów</li>
-        <li>podgląd ostatnio oglądanych ekwipunków</li>
-        <li>uzupełnienie brakujących przedmiotów</li>
-        <li>formularz, w którym można zgłaszać błędy i uwagi</li>
-      </ul>
-    </msg>
-  </div>
+  <section class="section">
+    <div class="container">
+      <msg header="O stronie">
+        <ul>
+          <li>można przeglądać i wyszukiwać przedmioty z gry Margonem</li>
+          <li>można budować swój ekwipunek</li>
+          <li>można udostępniać swój ekwipunek poprzez link</li>
+          <li>można porównać swój ekwipunek z odwiedzanym</li>
+        </ul>
+      </msg>
+      <msg header="Nadchodzące zmiany">
+        <ul>
+          <li>porównywarka przedmiotów</li>
+          <li>podgląd ostatnio oglądanych ekwipunków</li>
+          <li>uzupełnienie brakujących przedmiotów</li>
+          <li>formularz, w którym można zgłaszać błędy i uwagi</li>
+        </ul>
+      </msg>
+    </div>
+  </section>
 </template>
 
 <script>
