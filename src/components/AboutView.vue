7cc4d7a8b131eb77f2b0048d89c2645ebb8f24a9
eryk <me@eryk.io>
2018-02-10 12:46:31 +0100

    change AboutView

--- src/components/AboutView.vue
+++ src/components/AboutView.vue
@@ -3,11 +3,15 @@
     <msg header="O stronie">
       <ul>
         <li>można przeglądać i wyszukiwać przedmioty z gry Margonem</li>
-        <li>można budować ekwipunek</li>
+        <li>można budować swój ekwipunek</li>
+        <li>można udostępniać swój ekwipunek poprzez link</li>
+        <li>można porównać swój ekwipunek z odwiedzanym</li>
       </ul>
     </msg>
     <msg header="Nadchodzące zmiany">
       <ul>
+        <li>porównywarka przedmiotów</li>
+        <li>podgląd ostatnio oglądanych ekwipunków</li>
         <li>uzupełnienie brakujących przedmiotów</li>
         <li>formularz, w którym można zgłaszać błędy i uwagi</li>
       </ul>
