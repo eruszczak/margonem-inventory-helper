0b6882b72dfdac77250b29622f46458f9965afa4
eryk <me@eryk.io>
2018-02-15 21:21:59 +0100

    decrease search results length and add logo and favicon

--- src/api/items.js
+++ src/api/items.js
@@ -13,7 +13,7 @@ export const fetchMultipleItems = (slugs, cb, cbError) => {
 }
 
 export const searchItems = (query, cb, cbError) => {
-  axios.get(`/api/item/?per_page=30&n=${query}`).then(cb, cbError)
+  axios.get(`/api/item/?per_page=15&n=${query}`).then(cb, cbError)
 }
 
 export const fetchHelpers = (cb, cbError) => {
