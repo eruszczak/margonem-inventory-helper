fd91834c826aa3bb1c76301ab14b35590ec92968
Eryk Ruszczak <erykruszczak@gmail.com>
2019-09-08 18:47:19 +0200

    Env stuff.

--- backend/margonem/settings.py
+++ backend/margonem/settings.py
@@ -71,10 +71,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
+STATIC_ROOT = os.environ.get('STATIC_ROOT', os.path.join(os.path.dirname(BASE_DIR), 'static'))
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media')
+MEDIA_ROOT = os.environ.get('MEDIA_ROOT', os.path.join(os.path.dirname(BASE_DIR), 'media'))
 
 CACHES = {
     'default': {
